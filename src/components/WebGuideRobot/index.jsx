8abe8b572a760e32d9c45249b91e66c0b085e09d
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2024-12-13 22:12:36 -0300

    feat: add bot question/answer logic

--- src/components/WebGuideRobot/index.jsx
+++ src/components/WebGuideRobot/index.jsx
@@ -1,63 +1,99 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Lottie from 'lottie-react';
-import animationData from '../../assets/hiRobot.json';
-import './styles.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { config, messageResponses } from "../../config/ChatBotCfg";
+import Lottie from "lottie-react";
+import animationData from "../../assets/hiRobot.json";
+import "./styles.css";
 
-const WebGuideRobot = React.forwardRef(({location}, ref) => {
-   
-    const { wgrLocation, scrollDirection, predominantSection } = location
+const WebGuideRobot = React.forwardRef(({ location }, ref) => {
+   const [question, setQuestion] = useState("");
+   const [answer, setAnswer] = useState(config.initialMessage);
 
-    console.log("wgrLocation:", wgrLocation)
-    console.log("scrollDirection:", scrollDirection)
+   const { wgrLocation, scrollDirection, predominantSection } = location;
 
-        let animationState = "wgrobot--initialPos"
-            
-        if (wgrLocation === 'cta') {
-            animationState = (scrollDirection === 'down') 
-                ? "wgrobot--goToCTA" 
-                : "wgrobot--stayEnd";
+   console.log("wgrLocation:", wgrLocation);
+   console.log("scrollDirection:", scrollDirection);
 
-        } else if (wgrLocation === 'timeline') {
-            animationState = (scrollDirection === 'down') 
-                ? "wgrobot--goToTimeline" 
-                : "wgrobot--returnFromCTA";
+   let animationState = "wgrobot--initialPos";
 
-        } else if (wgrLocation === 'mainInfo' && predominantSection === 'mainInfo') {
-            animationState = (scrollDirection === 'down')
-                ? "wgrobot--goToTimeline"
-                : "wgrobot--returnFromTimeline";
-        }
-        
+   const handleAnswer = (question) => {
+      
+      const lowerCaseQuestion = question.toLowerCase();
+      
+      let maxScore = 0;
+      let bestMatchKey = null;
+      
+      Object.keys(messageResponses).forEach((key) => {
+         
+         let words = key.split(" ");
+         
+         let score = words.reduce((acc, word) => {
 
-    return (
-        <>
-            <div
-                className={`wgrobot__container ${animationState}`}
-                ref={ref}
-            >
-                <Lottie className="wgrobot" animationData={animationData} />
+               return lowerCaseQuestion.includes(word)
+                  ? acc + 1
+                  : acc;
+            },0)
 
+         if (score > maxScore) {
+            maxScore = score;
+            bestMatchKey = key;
+         }
+      })
+
+      setAnswer(messageResponses[bestMatchKey] || messageResponses.default);
+
+   };
+
+   const handleKeyDown = (e) => {
+      if (e.key === "Enter") {
+         handleAnswer(question);
+      }
+   }
+
+
+   if (wgrLocation === "cta") {
+      animationState =
+         scrollDirection === "down" ? "wgrobot--goToCTA" : "wgrobot--stayEnd";
+   } else if (wgrLocation === "timeline") {
+      animationState =
+         scrollDirection === "down"
+            ? "wgrobot--goToTimeline"
+            : "wgrobot--returnFromCTA";
+   } else if (wgrLocation === "mainInfo" && predominantSection === "mainInfo") {
+      animationState =
+         scrollDirection === "down"
+            ? "wgrobot--goToTimeline"
+            : "wgrobot--returnFromTimeline";
+   }
+
+   return (
+      <div className={`wgrobot__container ${animationState}`} ref={ref}>
+         <Lottie className="wgrobot" animationData={animationData} />
+
+         <div className="chat__container" ref={ref}>
+            <p>Respuesta: {answer}</p>
+
+            <div className="input__container">
+               <input
+                  type="text"
+                  value={question}
+                  onChange={(e) => setQuestion(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                  placeholder="Hazme una pregunta..."
+               />
+               <button
+                  onClick={() => {
+                     handleAnswer(question);
+                  }}
+               >Preguntar</button>
             </div>
-            <div 
-                className="chat__container"
-                ref={ref}
-                >
-                <p>Respuesta: {'answer'}</p>
-                <input
-                    type="text"
-                    value={'question'}
-                    onChange={(e) => setQuestion(e.target.value)}
-                    placeholder="Hazme una pregunta..."
-                    />
-                <button>Preguntar</button>
-            </div>
-        </>
-    )
-})
+         </div>
+      </div>
+   );
+});
 
 WebGuideRobot.propTypes = {
-    location: PropTypes.object
-}
+   location: PropTypes.object,
+};
 
-export default WebGuideRobot
+export default WebGuideRobot;
---
+++ src/config/ChatBotCfg.js
@@ -0,0 +1,27 @@
+const botName = "Botie";
+
+const messageResponses = {
+    bienvenido: `Hola, soy ${botName}. Fui creado por Pablo y estoy programado para contestar sobre algunos aspectos personales y profesionales sobre él. ¿Qué deseas saber?`,
+    hola: "¡Hola! ¿Puedo ayudarte?",
+    adios: "¡Hasta la próxima!",
+    chau: "¡Hasta luego!",
+    gracias: "De nada!",
+    tal: "Estoy bien y usted?",
+    frontend: `En el frontend, mi herramienta preferida es ReactJS. Con ella, creo interfaces de usuario modernas y dinámicas que mejoran la experiencia del usuario. Además, manejo JavaScript puro ("vanilla") para construir soluciones ligeras y optimizadas, sin depender de bibliotecas adicionales. Si tienes alguna duda sobre estas tecnologías o cómo las utilizo, ¡pregunta sin problema!`,
+    backend: `En el backend, trabajo principalmente con Spring Boot, una plataforma robusta y eficiente para desarrollar servicios. Utilizo JPA e Hibernate para la gestión de datos y la persistencia, lo que me permite crear APIs RESTful escalables y seguras. Siempre estoy enfocado en optimizar el rendimiento y asegurar que las aplicaciones puedan manejar una gran cantidad de usuarios simultáneamente. Si te interesa saber más sobre cómo desarrollo APIs, ¡pregúntame!`,
+    arquitectura: `Mi enfoque en la arquitectura de software es siempre crear soluciones escalables y sostenibles. Utilizo principios como la separación de responsabilidades, la modularidad y el desacoplamiento de servicios para asegurar que el software sea fácil de mantener a largo plazo. En mis proyectos, trato de balancear la eficiencia del código con la facilidad de ampliación y mantenimiento. Siempre me aseguro de que el sistema sea flexible y pueda adaptarse al crecimiento de las necesidades del negocio.`,
+    metodologias: `En mis proyectos, aplico metodologías ágiles como Scrum y Kanban para gestionar el trabajo y asegurar una entrega continua de valor. Esto me permite tener ciclos de desarrollo cortos y ajustarme rápidamente a los cambios de requisitos. También hago un uso intensivo de pruebas unitarias e integración continua para garantizar que cada parte del sistema funcione correctamente antes de ser integrada al resto del proyecto. La calidad del código y la rapidez de entrega son siempre mis prioridades.`,
+    herramientas: `Mi flujo de trabajo está respaldado por un conjunto de herramientas que optimizan tanto el desarrollo frontend como backend. Para crear interfaces de usuario dinámicas y optimizadas, utilizo ReactJS junto con herramientas como Webpack y Babel, que me permiten gestionar el bundling y la transpilación del código. En el backend, Spring Boot es mi marco de trabajo principal para crear APIs robustas, mientras que utilizo JPA e Hibernate para gestionar la persistencia de datos de manera eficiente. Para mantener la calidad y la estabilidad del código, empleo herramientas de testing como JUnit en el backend y pruebas de integración para asegurar que todo el sistema funcione perfectamente. Además, con Git y GitHub gestiono el código fuente y colaboro en equipo.`,
+    proyectos: `He trabajado en una variedad de proyectos, desde aplicaciones web interactivas hasta APIs RESTful robustas. En un proyecto reciente, diseñé una plataforma de gestión de tareas utilizando ReactJS en el frontend y Spring Boot en el backend, con JPA para la persistencia. El sistema fue escalado para soportar miles de usuarios simultáneos. Si deseas conocer más detalles sobre mis proyectos o ejemplos específicos, ¡no dudes en preguntar!`,
+    aprendizajes: `Me encanta aprender y constantemente busco mejorar mis habilidades. Algunos de los últimos temas que he estudiado incluyen el uso de microservicios en Spring Boot, la optimización de rendimiento en aplicaciones ReactJS, y las mejores prácticas en la implementación de APIs RESTful. También me interesa explorar nuevas herramientas y tecnologías según las necesidades planteadas.`,
+    retos: `Uno de los mayores retos que enfrento como desarrollador es mantener un equilibrio entre la velocidad de desarrollo y la calidad del software. Aunque siempre busco entregar soluciones de manera rápida y eficiente, también me esfuerzo por asegurarme de que el código sea limpio, bien documentado y fácil de mantener a largo plazo. Además, enfrentarme a nuevos desafíos tecnológicos y estar al día con las mejores prácticas es algo que me mantiene motivado.`,
+    default: "Lo siento, no entendí eso.",
+}
+
+const config = {
+  botName: botName,
+  initialMessage: messageResponses.welcome 
+}
+
+
+export { config, messageResponses };
