fa60d582a7989e499e27fa1e60e1c4ecc3aa740c
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-01-09 15:14:05 -0300

    feat: add click to visit functionality for proyects previews

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -14,7 +14,9 @@ const Projects = React.forwardRef((props, ref) => {
                     <p className='projects__description'>Sitio web dinámico desarrollado siguiendo el dieño de un equipo UX/UI, implementando metodología pixel perfect.<br/>Incluye animaciones avanzadas con CSS y JavaScript (Intersection Observer API) para lograr una experiencia visual fiel al diseño original.<br/>Contiene formulario de contacto, solicitud de turnos online y selección de idioma EN/ES.</p>
                 </div>
                 <div className="projects__image">
-                    <img className='projects__img' src={ProjectImg1} alt="Proyect Image" />
+                    <a href="https://backlab-strategies.vercel.app" target="_blank" className='projects__link'>
+                        <img className='projects__img' src={ProjectImg1} alt="Proyect Image" />
+                    </a>
                 </div>
             </div>
             <div className="projects__row">
