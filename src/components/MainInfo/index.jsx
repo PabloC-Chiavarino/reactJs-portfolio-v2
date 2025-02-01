0b13e095ea9542ed43c8ce6c1e41823fc84e7429
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-01-31 22:13:40 -0300

    refactor: move MainInfo content to config file for code maintainability

--- src/components/MainInfo/index.jsx
+++ src/components/MainInfo/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { mainInfoData } from '../../config/mainInfoData'
 import './styles.css'
 
 const MainInfo = React.forwardRef((props, ref) => {
@@ -7,13 +7,12 @@ const MainInfo = React.forwardRef((props, ref) => {
     return (
         <section className="mainInfo" id="mainInfo" ref={ref}>
             <div className="mainInfo__title">
-                <h1>Pablo C. Chiavarino</h1> 
-                <h2>Desarrollador <span className='text-highlightB' style={{fontWeight: 400}}>Full Stack</span></h2>
+                {mainInfoData.es.title} 
+                {mainInfoData.es.subtitle}
             </div>
-            <h3>¡Hola y <span className='text-highlightB'>bienvenido!</span></h3>
+            {mainInfoData.es.greeting}
             <div className="mainInfo__info">
-                <p>Me dedico al desarrollo de soluciones tecnológicas completas, abarcando tanto la <span className='text-highlightA'>creación de interfaces</span> como la <span className='text-highlightA'>implementación de servicios.</span></p><br/><p>En el lado del frontend, utilizo principalmente ReactJS para <span className='text-highlightA'>crear aplicaciones web interactivas y dinámicas</span>, aplicando principios de diseño de interfaces como SOLID, KISS, patrones como pixel-perfect y<span className='text-highlightA'> modularidad en los componentes</span>. Mi enfoque se basa organización eficiente de estilos con CSS puro, sin uso de librerias, a menos que sea requerido. También tengo experiencia en desarrollo con JavaScript "vanilla" para <span className='text-highlightA'>construir soluciones ligeras y eficientes.</span><br/><br/></p><p> En el backend, tengo experiencia en la construcción de APIs y servicios utilizando Spring Boot, así como en JPA e Hibernate para la gestión de datos y persistencia. Utilizo Java como lenguaje principal para <span className='text-highlightA'>desarrollar soluciones robustas y eficientes</span>, empleando técnicas como abstracciones genéricas, mappers y DTOs para mantener un código limpio, escalable y fácilmente mantenible.</p><br/>
-                <p>Mi meta se centra en ofrecer software de <span className='text-highlightB'>arquitectura sólida y escalable</span>, priorizando la eficiencia en el rendimiento y la facilidad de mantenimiento en ambos, frontend y backend.</p>
+                {mainInfoData.es.description}
             </div>
         </section>           
     )
